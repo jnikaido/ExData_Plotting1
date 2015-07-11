07accfb2a2549106b1897a574f79ba922b5b22c1
jnikaido <jnikaido@gmail.com>
2015-07-11 03:10:12 -1000

    Revised Plot1

--- plot1.R
+++ plot1.R
@@ -2,28 +2,25 @@
 ## 
 
 ## Read in the data file, assuming it's in the current working directory
-datafile <- read.csv("household_power_consumption.txt", header=TRUE, sep=";",
-                     na.strings = "?", quote='\"')
-
-## convert the date format in the data file
-datafile$Date <- as.Date(datafile$Date, format="%d/%m/%Y")
+datafile <- read.table("household_power_consumption.txt", header=TRUE,
+                       sep=";", na="?")
 
 ## only use the data from 2007-02-01 through 2007-02-02
-dataset <- subset(datafile, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+filter <- datafile$Date == "1/2/2007" | datafile$Date == "2/2/2007"
 
-## don't need the original file, so release from memory
-rm(datafile)
+dataset <- datafile[filter, ]
 
-## convert date and time formats
-datetime <- paste(as.Date(dataset$Date), dataset$Time)
-dataset$datetime <- strptime(datetime, format="POSIXct")
+## convert the date and time values to make a single column
+datetime <- paste(dataset$Date, dataset$Time)
+dataset$DateTime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 
 ## Plot 1 Output
-hist(dataset$Global_active_power,main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
 
 ## Create PNG File
-dev.copy(png, file="plot1.png", height=480, width=480)
+png(file="plot1.png",height=480,width=480)
+hist(dataset$Global_active_power,main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.off()
 
 
