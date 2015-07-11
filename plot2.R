527d53572eca544d7bad389d3c4e8d6cc4c5d37e
jnikaido <jnikaido@gmail.com>
2015-07-11 03:12:07 -1000

    Original commit didn't have saved version of R script

--- plot2.R
+++ plot2.R
@@ -6,16 +6,16 @@ datafile <- read.table("household_power_consumption.txt", header=TRUE,
                        sep=";", na="?")
 
 ## only use the data from 2007-02-01 through 2007-02-02
-filter <- Date == "1/2/2007" | Date == "2/2/2007"
+filter <- datafile$Date == "1/2/2007" | datafile$Date == "2/2/2007"
 
 dataset <- datafile[filter, ]
 
 ## convert the date and time values to make a single column
-datetime <- paste(Date, Time)
+datetime <- paste(dataset$Date, dataset$Time)
 dataset$DateTime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 
 ## Draw the plot to plot2.png
 png(filename = "plot2.png", width=480,height=480)
-plot(DateTime, globalActivePower, type="l",
+plot(dataset$DateTime, dataset$Global_active_power, type="l",
      xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
